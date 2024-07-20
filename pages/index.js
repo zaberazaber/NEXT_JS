573ec9b8ed66901b989730b599697f2fd39cf05d
zaberazaber <zubairansarijmi@gmail.com>
2024-07-20 19:04:27 +0530

    fixed linting errors

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div>
+    <div  style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
       <h1>Welcome to the Chrome-only Application</h1>
       <p>This application is accessible only through the Google Chrome browser.</p>
     </div>
